38aa58bd827672d6025da0fa6ef15563712ce329
Richie Casto <rcasto92@gmail.com>
2016-07-01 01:49:36 -0700

    Delete templates.js

--- templates.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var template = document.querySelector('#testTemplate');
-    var host = document.querySelector('#shadowHost');
-    var root = host.createShadowRoot();
-    host.appendChild(document.importNode(template.content, true));
-    root.appendChild(document.importNode(template.content, true));
-}());
